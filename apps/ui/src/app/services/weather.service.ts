b7e54540ac6e777ce3ef468a003669a536faed9f
CK Ng <ngchoonkhon@gmail.com>
2024-12-08 17:05:05 +0800

    add ui docker

--- apps/ui/src/app/services/weather.service.ts
+++ apps/ui/src/app/services/weather.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IIpInfo, IWeather } from '../model/weather.type';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { BehaviorSubject, catchError } from 'rxjs';
 @Injectable({
   providedIn: 'root',
---
+++ apps/ui/src/environments/environment.docker.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://weather-forecast.127.0.0.1.nip.io/api',
+};
